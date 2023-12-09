1a21af5ed9647c2450b45b3f51e40b1e81d21ed5
MsWakesho <wakeshosonja@gmail.com>
2023-12-09 21:19:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const sinon = require('sinon');
+
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  describe('index.js', () => {
+    let spy;
+  
+    beforeEach(() => {
+      spy = sinon.spy(console, 'log');
+    });
+  
+    afterEach(() => {
+      spy.restore();
+    });
+})
+
